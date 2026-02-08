d081f7c74978b404e9ab2ae1f00992c70ee31dbf
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-02-08 16:38:57 +0430

    fix(styles): Add py to section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import AuthCard from './components/AuthCard'
+import LoginForm from './components/LoginForm'
+import RegisterForm from './components/RegisterForm'
 
 function App() {
   const [tab, setTab] = useState("login")
@@ -14,7 +16,40 @@ function App() {
       }
 
   return (
-    <AuthCard title={header.title}></AuthCard>
+    <AuthCard title={header.title}>
+      <div className='flex justify-between mb-4'>
+        <button
+          onClick={() => setTab("login")}
+          className={`w-full rounded p-2 font-semibold
+            ${tab === "login"
+              ? "bg-green-800 text-white"
+              : "bg-green-100 text-gray-800"
+            }`}
+        >
+          Login
+        </button>
+        <button
+          onClick={() => setTab("register")}
+          className={`w-full p-2 rounded font-semibold
+            ${tab === "register"
+              ? "bg-green-800 text-white"
+              : "bg-green-100 text-gray-800"
+            }`}
+        >
+          Register
+        </button>
+      </div>
+
+      {tab === "login" ? (
+        <LoginForm
+          key="login"
+          onSwitchToRegister={() => setTab("register")} />
+      ) : (
+        <RegisterForm
+          key="register"
+          onSwitchToLogin={() => setTab("login")} />
+      )}
+    </AuthCard>
   )
 }
 
