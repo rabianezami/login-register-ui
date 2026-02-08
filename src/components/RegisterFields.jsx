d4c06f3bfccbb19b90c240c0d13c493df678aaac
rabia zia nezami <rabianezami786@gail.com>
2026-02-08 20:47:00 +0430

    feat: Add terms dev & eye Icon instade of message box

--- src/components/RegisterFields.jsx
+++ src/components/RegisterFields.jsx
@@ -1,9 +1,11 @@
 import InputField from "./InputField";
+import { FiEye, FiEyeOff } from "react-icons/fi";
 
-export default function RegisterFields({register, errors, showPassword, setShowPassword}) {
+export default function RegisterFields({ register, errors, showPassword, setShowPassword }) {
+
   return (
     <>
-      <InputField 
+      <InputField
         label="Full Name"
         id="reg-fullName"
         type="text"
@@ -13,7 +15,7 @@ export default function RegisterFields({register, errors, showPassword, setShowP
         autoComplete="name"
       />
 
-      <InputField 
+      <InputField
         label="Email"
         id="reg-email"
         type="email"
@@ -23,7 +25,7 @@ export default function RegisterFields({register, errors, showPassword, setShowP
         autoComplete="email"
       />
 
-      <InputField 
+      <InputField
         label="Password"
         id="reg-password"
         type={showPassword ? "text" : "password"}
@@ -31,9 +33,18 @@ export default function RegisterFields({register, errors, showPassword, setShowP
         register={register("password")}
         error={errors.password?.message}
         autoComplete="new-password"
-      />
+      >
 
-      <InputField 
+        <button
+          type="button"
+          className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 "
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          {showPassword ? <FiEyeOff /> : <FiEye />}
+        </button>
+      </InputField>
+
+      <InputField
         label="Confirm password"
         id="reg-confirm-password"
         type={showPassword ? "text" : "password"}
@@ -41,18 +52,33 @@ export default function RegisterFields({register, errors, showPassword, setShowP
         register={register("confirmPassword")}
         error={errors.confirmPassword?.message}
         autoComplete="new-password"
-      />
+      >
 
-      <div className="flex items-center justify-between text-xs mb-4">
-        <label className="flex items-center gap-2 cursor-pointer">
-            <input 
-              type="checkbox"
-              checked={showPassword}
-              onChange={(e) => setShowPassword(e.target.checked)}
-            />
-            Show password
+        <button
+          type="button"
+          className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          {showPassword ? <FiEyeOff /> : <FiEye />}
+        </button>
+      </InputField>
+
+      <div className="mb-4">
+        <label className="flex items-center gap-2 text-xs cursor-pointer">
+          <input
+            type="checkbox"
+            {...register("terms")}
+          />
+          I agree to the Terms & conditions
         </label>
+
+        {errors.terms && (
+          <p>
+            {errors.terms?.message}
+          </p>
+        )}
       </div>
+
     </>
   )
 }
